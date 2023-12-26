baeb21f04fdf429f632c342988d1f74c39e3733f
dmieter <dmieter@yahoo.com>
2023-12-26 14:08:46 +0300

    styles + data

--- doc-serve.py
+++ doc-serve.py
@@ -1,3 +1,4 @@
+import os, os.path
 import cherrypy
 from langchain.embeddings import HuggingFaceEmbeddings
 import chromadb
@@ -17,22 +18,40 @@ print("Size:" + str(vectorstore._collection.count()))
 
 class AnswerQuestion(object):
     @cherrypy.expose
-    def index(self):
+    def index(self, question = "", answer = ""):
+        question_title = ' Док, ' if len(question) > 0 else ''
         return """<html>
-          <head></head>
+          <head>
+            <link rel="stylesheet" href="static/css/doc.css">
+          </head>
           <body>
-            <form method="get" action="generate">
+            <form method="get" action="ask">
               <input type="text" value="" name="question" />
-              <button type="submit">Initiate the sending...</button>
+              <button type="submit">Задать вопрос</button>
             </form>
+            <img src="static/images/doc-logo.jpg" width="200" height="200" border="-5">
+              <p>>{}{}</p>
+              <p>> {}</p>
           </body>
-        </html>"""
+        </html>""".format(question_title, question, answer)
 
     @cherrypy.expose
-    def generate(self, question=""):
-        res_docs = vectorstore.similarity_search(question)
-        return "Answer for {} is: {}".format(question, res_docs[0].page_content)
+    def ask(self, question=""):
+        trimmed_question = question[:300] if len(question) > 300 else question
+        res_docs = vectorstore.similarity_search(trimmed_question)
+        return self.index(question, res_docs[0].page_content[:1000])
 
 
 if __name__ == '__main__':
-    cherrypy.quickstart(AnswerQuestion())
+    work_dir = os.path.abspath(os.getcwd())
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': work_dir
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(work_dir, "docbot", "static"),
+        }
+    }
+    cherrypy.quickstart(AnswerQuestion(), '/', conf)
