0f478645f3aa9104903feb5685dd549e18b3b770
dmieter <dmieter@mail.ru>
2024-02-03 10:50:14 +0000

    next iteration update

--- doc_bot.py
+++ doc_bot.py
@@ -3,21 +3,106 @@ import telebot
 from langchain.prompts import PromptTemplate
 import chromadb
 from langchain.vectorstores import Chroma
+from datetime import datetime
+from telebot import types
 import doc_core as dc
+import yaml
 
 BOT_TOKEN = os.environ.get('DOCBOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
+config = {}
+answer_chains = {}
+retrievers = {}
 
-obrpravo_prompt = PromptTemplate.from_template(dc.prompt_obr_pravo)
-obrpravo_answer_chain = dc.prepareAnswerChain("./chroma_db", "mpei_obrpravo", dc.embeddings, dc.llm, obrpravo_prompt)
+
+def load_config():
+  global config
+  
+  with open('config/config.yml', 'r') as file:
+      config = yaml.safe_load(file)
+      print("config reloaded: {}".format(config))
+
+def get_config_value(key, category, default_category): 
+  if key in category.keys():
+    return category[key]
+  else:
+    return default_category[key]
+
+def init():
+  load_config()
+  answer_chains.clear()
+  retrievers.clear()
+  
+
+  for category_name in config['categories'].keys():
+    category = config['categories'][category_name]
+
+    prompt_template_name = get_config_value('prompt', category, config['default'])
+    prompt_template = dc.prompts[prompt_template_name]
+    prompt = PromptTemplate.from_template(prompt_template)
+
+    display_name = category['display_name']
+    db_path = get_config_value('db_path', category, config['default'])
+    retriever = get_config_value('retriever', category, config['default'])
+
+    answer_chain, retriever = dc.prepareAnswerChain(db_path, category_name, dc.embeddings, dc.llm, prompt, threshold=retriever['threshold'], k=retriever['k'], s=retriever['s'], t=retriever['t'])
+    answer_chains[display_name] = answer_chain
+    retrievers[display_name] = retriever
+
+
+@bot.message_handler(commands=['reload'])
+def reload(message):
+  init()
+  bot.reply_to(message, "База знаний обновлена")
+
+@bot.message_handler(commands=['category'])
+def set_knowledge_base(message, greeting = "Выберите категорию знаний"):
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  for key in answer_chains.keys():
+    btn = types.KeyboardButton(key)
+    markup.add(btn)
+  bot.send_message(message.from_user.id, greeting, reply_markup=markup)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  set_knowledge_base(message, """🖖 Привет! Я док, специалист по различным документам и приказам МЭИ! 
+Рекомендую сразу выбрать интересующую категорию знаний
+В дальнейшем для выбора категории можно использовать команду /category""")
+
+
+def prepare_answer(question, knowledge):
+
+  if knowledge in answer_chains.keys():
+    answer = answer_chains[knowledge].invoke(question)
+  else:
+    answer = "Документы по категории {} еще готовятся".format(knowledge)
+
+  print("Retrieved docs: {}".format(dc.format_docs(retrievers[knowledge].get_relevant_documents(question))))
+
+  return answer
 
 
 @bot.message_handler(func=lambda msg: True)
 def general_question(message):
-  question = message.text
-  trimmed_question = question[:400] if len(question) > 400 else question
-  answer = obrpravo_answer_chain.invoke(trimmed_question)
-  bot.reply_to(message, answer)
 
+  if message.text in answer_chains.keys():
+    bot.unpin_all_chat_messages(chat_id = message.chat.id)
+    bot.pin_chat_message(chat_id = message.chat.id, message_id = message.message_id)
+    bot.reply_to(message, "Документы готовы")
+  else:
+    chat = bot.get_chat(message.chat.id)
+    question = message.text
+    print(str(datetime.now()) + " " + str(message.chat.id) + " Q: " + question)
+
+    trimmed_question = question[:800] if len(question) > 800 else question
+    knowledge_base = chat.pinned_message.text if chat.pinned_message else config['default']['knowledge_base']
+    answer = prepare_answer(trimmed_question, knowledge_base)
+    
+    print(str(message.chat.id) + " A: " + answer)
+    suffix = """\n*{}.*""".format(knowledge_base)
+    bot.reply_to(message, answer + suffix, parse_mode = 'Markdown')
+
+init()
+print(str(datetime.now()) + " Doc is here!")
 bot.infinity_polling()
--- doc_core.py
+++ doc_core.py
@@ -5,12 +5,21 @@ from langchain.chat_models import GigaChat
 from langchain import hub
 from langchain.schema import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableParallel
 from langchain.prompts import PromptTemplate
+from langchain.chains import create_citation_fuzzy_match_chain
 
-EMBEDDINGS_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-CONTEXT_DOC_NUMBER = 3
+from langchain.schema.vectorstore import VectorStoreRetriever
+from langchain.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain.schema.document import Document
+from typing import List
+
+from datetime import datetime
+
+#MBEDDINGS_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+CONTEXT_DOC_NUMBER = 5
 #EMBEDDINGS_MODEL = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
-#EMBEDDINGS_MODEL = "cointegrated/rubert-tiny2"
+EMBEDDINGS_MODEL = "cointegrated/rubert-tiny2"
 
 prompt_template_eng = """Use the following pieces of context to answer the question at the end.
       If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -19,36 +28,90 @@ prompt_template_eng = """Use the following pieces of context to answer the quest
       {context}
       Question: {question}
       Helpful Answer:"""
-prompt_template_ru = """Действуй как секретарь кафедры технического университета.
-      Используй для ответа на вопрос приведенные ниже документы.
-      Если данных для ответа не хватает, то явно укажи на это в ответе.
-      Добавь в ответ дополнительные релевантные подробности из документов.
+prompt_template_ru = """Действуй как высококлассный юрист-специалист и используй для ответа на вопрос приведенные ниже документы.
+      Ответ должен содержать максимум подробностей из приведенных документов.
       От точности ответа зависит качество образования в стране!
       Документы: {context}
-      Вопрос: {question}
-      Ответ секретаря:"""
-prompt_obr_pravo = """Действуй как юрист-специалист и используй для ответа на вопрос приведенные ниже документы.
+      Вопрос: {question}; В каких документах можно найти детали?
+      Ответ специалиста:"""
+
+prompt_mpei_orders = """Действуй как высококлассный юрист-секретарь кафедры и используй для ответа на вопрос приведенные ниже документы.
+      От подробности ответа зависит качество образования в стране!
+      Если данных для ответа не хватает, то дай рекомендацию, как лучше уточнить вопрос.
+      Документы: {context}
+      Вопрос: {question}; В каком приказе можно посмотреть детали?
+      Ответ специалиста:"""
+
+prompt_obr_pravo = """Действуй как высококлассный юрист-специалист и используй для ответа на вопрос приведенные ниже документы.
       Если данных для ответа не хватает, то дай рекомендацию, как лучше уточнить вопрос.
       Ответ должен содержать максимум подробностей из приведенных документов.
       От точности ответа зависит качество образования в стране!
       Документы: {context}
-      Вопрос: {question}
+      Вопрос: {question}; В каком документе можно посмотреть детали?
       Ответ специалиста:"""
 
+prompts = {"general" : prompt_template_ru, "mpei_orders" : prompt_mpei_orders, "obr_pravo" : prompt_obr_pravo}
 
 #custom_prompt = PromptTemplate.from_template(prompt_template_ru)
 #obrpravo_prompt = PromptTemplate.from_template(prompt_obr_pravo)
 
 
+class MyVectorStoreRetriever(VectorStoreRetriever):
+    # See https://github.com/langchain-ai/langchain/blob/61dd92f8215daef3d9cf1734b0d1f8c70c1571c3/libs/langchain/langchain/vectorstores/base.py#L500
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        docs_and_similarities = (
+            self.vectorstore.similarity_search_with_relevance_scores(
+                query, **self.search_kwargs
+            )
+        )
+
+        # Make the score part of the document metadata
+        for doc, similarity in docs_and_similarities:
+            doc.metadata["score"] = similarity
+            doc.metadata["len"] = len(doc.page_content)
+
+        retrieved_docs = [doc for doc, _ in docs_and_similarities]
+        result_docs = []
+        
+        score_num = self.search_kwargs["s"]
+        time_num = self.search_kwargs["t"]
+        
+        if len(retrieved_docs) > score_num and len(retrieved_docs) > time_num:
+            docs_by_score = retrieved_docs
+            result_docs.extend(docs_by_score[:score_num])
+            
+            docs_by_date = sorted(docs_by_score[score_num:], key=lambda d: d.metadata['date'], reverse=True) 
+            result_docs.extend(docs_by_date[:time_num])
+        
+        
+        else:
+            result_docs = retrieved_docs
+        
+        return result_docs
+
+    
+
+
+QA_COURCES_DICT = {}
+
+def get_doc_with_description(doc):
+    return "Название Документа: {} от {}\n Содержимое:{}".format(doc.metadata['name'], doc.metadata['date'], doc.page_content)
 
 def format_docs(docs):
+    head = "Сегодня {}\n =================== \n".format(datetime.today().strftime('%Y%m%d'))
     docs_num = CONTEXT_DOC_NUMBER if (len(docs) > CONTEXT_DOC_NUMBER) else len(docs)
-    return "\n =================== \n".join(docs[i].page_content for i in range(docs_num))
+    formatted_docs = head + "\n =================== \n".join(get_doc_with_description(docs[i]) for i in range(docs_num))
+    print(formatted_docs)
+    return formatted_docs
 
 
 embeddings = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL)
 llm = GigaChat(verify_ssl_certs=False)  
-def prepareAnswerChain(db_path, collection_name, embeddings, llm, prompt):
+
+                                                                    # retrieve r_k with score > threshold, pick r_s by score and then r_t by time
+def prepareAnswerChain(db_path, collection_name, embeddings, llm, prompt, threshold=0.185, k = 4, s = 2, t = 1):
   chroma_client = chromadb.PersistentClient(path=db_path)
   vectorstore = Chroma(
       client=chroma_client,
@@ -56,8 +119,14 @@ def prepareAnswerChain(db_path, collection_name, embeddings, llm, prompt):
       embedding_function=embeddings
   )
   print("Size:" + str(vectorstore._collection.count()))
+    
+  #retriever = vectorstore.as_retriever()      
+  retriever = MyVectorStoreRetriever(
+    vectorstore=vectorstore,
+    search_type="similarity_score_threshold",
+    search_kwargs={"score_threshold": threshold, "k": k, "s" : s, "t" : t}, # retrieve k with score > threshold, pick s by score and then t by time
+  )
 
-  retriever = vectorstore.as_retriever()      
   rag_chain = (
       {"context": retriever | format_docs, "question": RunnablePassthrough()}
       | prompt
@@ -65,7 +134,8 @@ def prepareAnswerChain(db_path, collection_name, embeddings, llm, prompt):
       | StrOutputParser()
   )
 
-  return rag_chain
+
+  return rag_chain, retriever
 
 
 #general_answer_chain = prepareAnswerChain("./chroma_db", "mpei_docs", embeddings, llm, custom_prompt)
