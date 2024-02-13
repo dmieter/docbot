ebadde235f139e8bb10e777f373666bbd33af48f
dmieter <32790897+dmieter@users.noreply.github.com>
2024-02-13 21:33:09 +0300

    updates

--- data/list_mpei_documents.py
+++ data/list_mpei_documents.py
@@ -16,10 +16,12 @@ def date_add_days(date_str, days):
     date_plus = date_obj + timedelta(days)
     return date_plus.strftime('%Y-%m-%d')
 
-def list_all_docs():
+def retrieve_all_docs(from_date):
     req = Request(MPEI_URL)
     response = urlopen(req).read()
 
+    docs = []
+    
     soup = bs(response, features='html.parser')
     links = soup.find_all('div', class_ = "link-item")
     print(len(links))
@@ -38,11 +40,31 @@ def list_all_docs():
             date_parts = date.split('.')
             date = date_parts[2]+"-"+date_parts[1]+"-"+date_parts[0]
 
-        expire_date = date_add_days(date, 120) # expires in 4 months by default
-        metadata = date + ";" + expire_date + ";" + filename + ";" + name + ";" + href
-        if date > "2024-01-01":
-            print(metadata)
-# %% TEST   
-list_all_docs()
+        div_desc = link.find('div', class_ = "description")
+        if div_desc:
+            desc = div_desc.text
 
-# use "uniq filename" for the result!
+        expire_date = date_add_days(date, 120) # expires in 4 months by default
+        if date > from_date:
+            docs.append((date, expire_date, filename, name ,href, desc))
+
+
+    return docs
+
+def list_all_docs(from_date):
+    links_set = set()
+    docs = retrieve_all_docs(from_date)
+
+    for date, expire_date, filename, name ,href, desc in docs:
+        metadata = date.replace(";", "") + ";" + expire_date.replace(";", "") + ";" + filename.replace(";", "") + ";" + name.replace(";", "") + ";" + href.replace(";", "") + ";" + desc.replace(";", "").replace("\r\n", " ")
+        links_set.add(metadata)
+
+    links_list = sorted(list(links_set), reverse=False)
+    for link in links_list:
+        print(link)    
+
+
+
+
+# %% TEST   
+list_all_docs("2024-01-01")
