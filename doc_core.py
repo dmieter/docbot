4ce53f073e2ad5c9d002debded82c41047ec1732
dmieter <dmieter@mail.ru>
2025-02-06 19:34:56 +0000

    specifying chroma query parameters

--- doc_core.py
+++ doc_core.py
@@ -82,12 +82,15 @@ def tokenize(s: str) -> list[str]:
 
 class MyVectorStoreRetriever(VectorStoreRetriever):
     # See https://github.com/langchain-ai/langchain/blob/61dd92f8215daef3d9cf1734b0d1f8c70c1571c3/libs/langchain/langchain/vectorstores/base.py#L500
+    
     def _get_relevant_documents(
         self, query: str, *, run_manager: CallbackManagerForRetrieverRun
     ) -> List[Document]:
+        chroma_valid_parameters = {'k', 'score_threshold'}
+        choma_search_kwargs = {k: v for k, v in self.search_kwargs.items() if k in chroma_valid_parameters}
         docs_and_similarities = (
             self.vectorstore.similarity_search_with_relevance_scores(
-                query, **self.search_kwargs
+                query, **choma_search_kwargs
             )
         )
 
